f5aebbb72d016df83e8aa67d9b68fa82c2e7e0de
hanxiaofei <568002633@qq.com>
2022-01-01 18:02:56 +0800

    first commit

---
+++ blog-view/src/components/Card.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="card">
+    <!--展示名片头部--头像名称-->
+    <div class="card-header">
+      <el-avatar :size="50" :src="user.avatar"></el-avatar>
+      <p style="margin-top: 0px;">Xingye</p>
+      <el-divider></el-divider>
+    </div>
+    <!--卡片底部--个人喜好-->
+    <div class="card-footer">
+      <el-collapse>
+        <el-collapse-item name="1" style="padding-left: 20px;padding-right: 20px" title="最喜欢的动漫">
+          <div>一人之下</div>
+        </el-collapse-item>
+        <el-collapse-item name="2" style="padding-left: 20px;padding-right: 20px" title="最喜欢的女孩子">
+          <div>她</div>
+        </el-collapse-item>
+        <el-collapse-item name="3" style="padding-left: 20px;padding-right: 20px" title="最喜欢的游戏">
+          <div>lol</div>
+        </el-collapse-item>
+        <el-collapse-item name="4" style="padding-left: 20px;padding-right: 20px" title="最喜欢的颜色">
+          <div>蓝色</div>
+        </el-collapse-item>
+      </el-collapse>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  data() {
+    return {
+      hasLogin: false,
+      fit: 'fill',
+      user: {
+        avatar: require('@/assets/tx.jpg')
+      },
+    }
+  },
+  methods: {
+    //跳转到个人github
+    goToGithub() {
+      window.location.href = "https://github.com/yubifeng"
+    }
+  },
+  created() {
+  }
+}
+</script>
+
+<style scoped>
+.card {
+  width: 250px;
+  padding: 20px 0px 0px 0px;
+  background-color: white;
+}
+.card-header {
+  text-align: center;
+}
+.card-footer {
+  margin-top: 10px;
+}
+.card:hover {
+  -webkit-box-shadow: #ccc 0px 10px 10px;
+  -moz-box-shadow: #ccc 0px 10px 10px;
+  box-shadow: #ccc 0px 10px 10px;
+}
+</style>
